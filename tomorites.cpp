5510e62832aa37c0c8efb4cb1e9b6122fca5c720
Kun Béla Roland <43463212+KBRoli@users.noreply.github.com>
2018-10-31 13:29:37 +0300

    Add files via upload

---
+++ tomorites.cpp
@@ -0,0 +1,16 @@
+#include "std_lib_faciities.h"
+
+int main()
+{
+ifstream f("nagy.txt");
+ofstream g("10mb.txt");
+char k;
+for(int i=0;i<=10*1024*1024;i++)
+}
+f.get(k);
+g<<k;
+
+{
+f.close();
+g.close();
+}
